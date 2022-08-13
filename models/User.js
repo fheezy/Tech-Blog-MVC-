9256a7521152dd018119dba5a4f4b851d7403037
fheezy <farhanhoque764@yahoo.com>
2022-08-13 14:43:12 -0500

    added bcrypt into user.js

---
+++ models/User.js
@@ -0,0 +1,33 @@
+const Sequelize = require('sequelize');
+const sequelizeConnection = require('../config/connection');
+const bcrypt = require('bcrypt');
+const User = sequelizeConnection.define('user', {
+
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true, 
+        autoIncrement: true,
+        allowNull: false
+    },
+    username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len:[3,30]
+            // make it 3-30 characteristic
+        }
+    },
+    password: {
+        type: Sequelize,STRING,
+        allowNull: false,
+        validate: {
+            len: [7,25] //password length will be atleast 7 and max is 25
+        }
+    }
+}, {
+    sequelize: sequelizeConnection,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'users'
+});
