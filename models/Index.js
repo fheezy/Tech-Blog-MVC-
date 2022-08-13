261b7c0b9cc260a3c43713b84d7c22e191616fc8
fheezy <farhanhoque764@yahoo.com>
2022-08-13 17:07:27 -0500

    created database

--- models/Index.js
+++ models/Index.js
@@ -2,7 +2,36 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
+// association that a One-To-Many relationship exist between A and B (foreign key defined)
+User.hasMany(Post, {
+    foreignKey: 'userID',
+    onDelete: 'CASCADE'
+});
 
+User.hasMany(Comment, {
+    foreignKey: 'userID',
+    onDelete: 'CASCADE',
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'userID',
+    onDelete: 'CASCADE',
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'postID',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'userID',
+    onDelete: 'CASCADE',
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'postID',
+    onDelete: 'CASCADE',
+});
 
 module.exports = {
     User, Post, Comment
